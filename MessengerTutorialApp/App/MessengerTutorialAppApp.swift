d6528b56fd661c3d1bf0d28d326f9c1692082382
LuaneEmiliano <102385819+LuaneEmiliano@users.noreply.github.com>
2023-10-17 10:59:50 -0700

    Initial Commit

---
+++ MessengerTutorialApp/App/MessengerTutorialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialAppApp.swift
+//  MessengerTutorialApp
+//
+//  Created by luane Niejelski on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
